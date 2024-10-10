9139f36e1380715759640a14d6d8b1edf7f0251f
Muhamad Fazza Muharam <muharramf25@gmail.com>
2024-10-10 14:44:54 +0700

    changed the login() function

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -7,35 +7,30 @@ const basePage = new BasePage()
 class LoginPage extends BasePage {
     constructor() {
         super()
-        this.loginLocatores = {
+        this.locators = {
             daftarAtauMasukButton : By.xpath('//ul[@class="nav align-items-center nav-ipad-pt"]/li[6]/a'),
-            email : By.xpath('//form[@id="form-login"]/div/input[1]'),
-            password : By.xpath('//form[@id="form-login"]/div[2]/input'),
+            emailInput : By.xpath('//form[@id="form-login"]/div/input[1]'),
+            passwordInput : By.xpath('//form[@id="form-login"]/div[2]/input'),
             loginButton : By.xpath('//form[@id="form-login"]/div[4]/button'),
             titleAccount : By.xpath('//h5[@class="title_akun"]')
         }       
     }
 
     async login() {
-        await basePage.clickButton(this.loginLocatores.daftarAtauMasukButton)
-        await driver.wait(until.elementLocated(this.loginLocatores.email), 0)
+        await basePage.clickButton(this.locators.daftarAtauMasukButton)
+        await driver.wait(until.elementLocated(this.locators.emailInput), 0)
 
-        const emailField = await driver.findElement(this.loginLocatores.email)
-        const passwordField = await driver.findElement(this.loginLocatores.password)
+        const emailField = await driver.findElement(this.locators.emailInput)
+        const passwordField = await driver.findElement(this.locators.passwordInput)
 
         await emailField.sendKeys((await user.createStandardUser()).email)
-        
-        await driver.wait(async function() {
-            const emailValue = await emailField.getAttribute('value');
-            return emailValue.length > 0;
-        }, 5000)
+        await basePage.waitForValue(emailField, 5000)
         
         await passwordField.sendKeys((await user.createStandardUser()).password)
+        await basePage.waitForValue(passwordField, 5000)
         
-        await driver.sleep(2000)
-        await basePage.clickButton(this.loginLocatores.loginButton)
-
-        await driver.wait(until.elementLocated(this.loginLocatores.titleAccount), 0)
+        await basePage.clickButton(this.locators.loginButton)
+        await driver.wait(until.elementLocated(this.locators.titleAccount), 0)
     }
 }
 
